b8f9f88689039e12fba77a1795b588887ecf734f
hmoazam <hannamoazam@gmail.com>
2024-12-19 00:13:26 +0000

    add all

---
+++ tests/predict/test_chain_of_thought.py
@@ -0,0 +1,14 @@
+import dspy
+from dspy import ChainOfThought
+from dspy.utils import DummyLM
+
+
+def test_initialization_with_string_signature():
+    lm = DummyLM([{"reasoning": "find the number after 1", "answer": "2"}])
+    dspy.settings.configure(lm=lm)
+    predict = ChainOfThought("question -> answer")
+    assert list(predict.predict.signature.output_fields.keys()) == [
+        "reasoning",
+        "answer",
+    ]
+    assert predict(question="What is 1+1?").answer == "2"
---
+++ tests/predict/test_program_of_thought.py
@@ -0,0 +1,36 @@
+import dspy
+from dspy import ProgramOfThought, Signature
+from dspy.utils import DummyLM
+
+
+class BasicQA(Signature):
+    question = dspy.InputField()
+    answer = dspy.OutputField(desc="often between 1 and 5 words")
+
+
+def test_pot_code_generation():
+    lm = DummyLM(
+        [
+            {"reasoning": "Reason_A", "generated_code": "```python\nresult = 1+1\n```"},
+            {"reasoning": "Reason_B", "answer": "2"},
+        ]
+    )
+    dspy.settings.configure(lm=lm)
+    pot = ProgramOfThought(BasicQA)
+    res = pot(question="What is 1+1?")
+    assert res.answer == "2"
+
+
+def test_pot_code_generation_with_error():
+    lm = DummyLM(
+        [
+            {"reasoning": "Reason_A", "generated_code": "```python\nresult = 1+0/0\n```"},
+            {"reasoning": "Reason_B", "generated_code": "```python\nresult = 1+1\n```"},
+            {"reasoning": "Reason_C", "answer": "2"},
+        ]
+    )
+    dspy.settings.configure(lm=lm)
+
+    pot = ProgramOfThought(BasicQA)
+    res = pot(question="What is 1+1?")
+    assert res.answer == "2"
