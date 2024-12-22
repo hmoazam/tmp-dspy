55e9a37984f03efa3cbcb0cfd8d7df4a73d92150
hmoazam <hmoazam@users.noreply.github.com>
2024-12-22 01:50:32 +0000

    Update versions

--- dspy/__metadata__.py
+++ dspy/__metadata__.py
@@ -1,7 +1,7 @@
 #replace_package_name_marker
-__name__="dspy"
+__name__="dspy-ai-hmoazam"
 #replace_package_version_marker
-__version__="2.5.43"
+__version__="5.11.38"
 __description__="DSPy"
 __url__="https://github.com/stanfordnlp/dspy"
 __author__="Omar Khattab"
